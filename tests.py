9399e6886834b579d1516207cf8fcbfd31c5b1ae
Jon Moshier <jonmoshier@gmail.com>
2017-03-02 10:07:26 -0700

    added pip freeze requirements

--- tests.py
+++ tests.py
@@ -28,7 +28,9 @@ class TestCases(unittest.TestCase):
         assert data['clicks']==0
         assert data['impressions']==0
         assert data['unique_user']==0
-
-
+    def test_badpost(self):
+        resp=self.app.post('/analytics?timestamp=asdasd&user=&event=ast13t913t_Qwr')
+        print(resp.status_code)
+        
 if __name__ == '__main__':
     unittest.main()
